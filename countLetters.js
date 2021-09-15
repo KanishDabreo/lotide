4cd5e939b592a8e099089dc07542a4b147e9b027
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-09-15 23:30:12 +0000

    this function takes in a sentence (as a string) and then returns a count of each of the letters in that sentence.

---
+++ countLetters.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+    // c.log Template Literals (Template Strings) version
+  } else if (actual !== expected) {
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const countLetters = function(countString){
+let countResults = {};
+for (let character of countString){
+  if(countResults[character]) {
+    countResults[character] += 1;
+  } else {
+    countResults[character] = 1;
+  }
+  }
+  return countResults;
+};
+console.log(countLetters('lighthouse in the house'));
+
+//previous example for extra help
+// const countOnly = function(allItems, itemsToCount) {
+//   const results = {}; 
+//   for (const item of allItems) {
+//     if (itemsToCount[item]) { 
+//       if (results[item]) {
+//         results[item] += 1;
+//       } else {
+//         results[item] = 1;
+//       }
+//     }
+//     console.log(item);
+//   }
+//   return results;
+// }
