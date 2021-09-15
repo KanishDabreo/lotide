3c5faea28a910be92d08c3cb6a530e78bd10910b
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-09-15 17:15:41 +0000

    completed tail assignment

--- tail.js
+++ tail.js
@@ -8,10 +8,34 @@ const assertEqual = function(actual, expected) {
 
 };
 
-const tail = function(arr){
-  return arr.slice(1);
+const tail = function(oldArray) {
+  let newArray = [];
+  for (let i = 0; i < oldArray.length - 1; i++) {
+  //loop through the array and tells it where to start
+    newArray[i] = oldArray[i + 1];
+  }
+  return newArray;
 };
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+/*extract the command-line arguments
+const args = process.argv;
+//console.log(args);
+
+//grabbing the tail elements as strings
+let x = args.slice(2);
+//console.log(x);
+
+//debug: to return elements of array as string
+//console.log(x[0], x[1])
+
+//add with placeholder parameters
+const sum = function(num1, num2) {
+
+  //Number() method to change strings to number
+  return Number(num1) + Number(num2);
+
+};
+console.log(sum(x[0], x[1]));*/
