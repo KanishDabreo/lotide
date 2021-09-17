92021a3ff6bf2ce7757bedfaff0fdf78c398a820
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-09-17 01:57:32 +0000

    takeUntil function needs to loop through the entire array until the callback returns a truthy value

---
+++ takeUntil.js
@@ -0,0 +1,21 @@
+//takeUntil function runs the first parameter array (data1) the and parameter is a function that will return truthy when the element in the array is < 0 causing the function to return the previous elements
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (const element of array) {
+    if (callback(element)) {
+      break;
+    }
+    results.push(element);
+  }
+  return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
