527b843f008aeebcce14635739e9bd834367a813
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-09-21 18:56:20 +0000

    adjustment

--- eqObjects.js
+++ eqObjects.js
@@ -45,21 +45,4 @@ const eqObjects = function (objOne, objTwo) {
   }
   return true;
  };
-
- // TEST CODE
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba), true); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc), false); // => false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
-
-
 module.exports = eqObjects;
